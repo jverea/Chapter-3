29bddf6740322c9a26a1d8e6076a37bcf22f6eca
jverea <jvereagovan2018@gmail.com>
2019-05-01 14:17:04 -0500

    update 5/1/19

--- src/Triangle.java
+++ src/Triangle.java
@@ -2,44 +2,31 @@ import java.awt.*;
 import java.util.Scanner;
 
 public class Triangle {
-
     public static void main(String[] args) {
+        int choice;
+        Scanner input = new Scanner(System.in);
+        System.out.println("whats type of tringle are you solving for? 1, 2, or 3");
+        System.out.println("1. Right triangle");
+        System.out.println("2. Isoceles");
+        System.out.println("3. Equaletral");
 
-        System.out.println("What type of triangle are you solving for?");
-        System.out.println("1. Right Triangle");
-        System.out.println("2. Isocles");
-        System.out.println("3. Equalteral");
+        choice = input.nextInt();
 
-        //choice = input.next.Int();
-
-
-            //tringle with two equal sides always try to find the base and area
-            Scanner input = new Scanner(System.in);
-            double sideA;
-            double sideB;
-            double sideC;
-            String sidechoice;
-            System.out.println("What side of the triangle are you trying to solve for? A, B, C");
-            sidechoice = input.next();
-
-            if (sidechoice.equals("A")) {
-                System.out.println("What is side b?>>> ");
-                sideB = input.nextDouble();
-                System.out.println("What is side c?>>> ");
-                sideC = input.nextDouble();
-                calucatesideA(sideB, sideC, sidechoice);
-            } else if (sidechoice.equals("B")) {
-            } else if (sidechoice.equals("C")) {
-            } else
-                System.out.println("That side does not exist on a tringle dummy!!!");
-        }
-
-        public static void calucatesideA ( double b, double c, String choice){
-            double a;
-            double squared;
-            a = Math.sqrt(c * c - b * b);
-            System.out.println("The side" + choice + "=" + a);
-        }
-
-
+        if (choice ==1);
+        rightTriangle();
+        else if (choice ==2);
+        isoclesTriangle();
+        else if (choice ==3);
+        equalateralTriangle();
+        else
+            triangleDoesNotExist();
     }
+    public static void isoclesTriangle(){
+    }
+public static void equalateralTriangle(){
+        //all sides are equal find the parimeter and hight
+}
+public static void rightTriangle(){
+        
+}
+}
