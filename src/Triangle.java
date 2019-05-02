761d91d373e419b35c17608ecf0803a880e7791f
jverea <jvereagovan2018@gmail.com>
2019-05-02 14:26:39 -0500

    update

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+
 import java.util.Scanner;
 
 public class Triangle {
@@ -12,21 +12,67 @@ public class Triangle {
 
         choice = input.nextInt();
 
-        if (choice ==1);
-        rightTriangle();
-        else if (choice ==2);
-        isoclesTriangle();
-        else if (choice ==3);
-        equalateralTriangle();
+        if (choice ==1)
+            rightTriangle();
+        else if (choice ==2)
+            isoclesTriangle();
+        else if (choice ==3)
+            equalateralTriangle();
         else
             triangleDoesNotExist();
     }
     public static void isoclesTriangle(){
+
+
     }
-public static void equalateralTriangle(){
+    public static void equalateralTriangle(){
         //all sides are equal find the parimeter and hight
 }
-public static void rightTriangle(){
-        
-}
+    public static void rightTriangle(){
+        Scanner input = new Scanner (System.in);
+        double sideA;
+        double sideB;
+        double sideC;
+        String sidechoice;
+
+        System.out.println("what side of the triangle are you trying to solve for? A, B, C");
+        sidechoice = input.next();
+
+        if (sidechoice.equals("A")){
+            System.out.println("what is side B? >>>");
+            sideB = input.nextDouble();
+            System.out.println("what is side C? >>>");
+            sideC = input.nextDouble();
+            calculateSideA_B(sideC, sideB, sidechoice);
+        }
+        else if(sidechoice.equals("B")){
+            System.out.println("what is side A? >>>");
+            sideA = input.nextDouble();
+            System.out.println("what is side C? >>>");
+            sideC = input.nextDouble();
+            calculateSideA_B(sideC, sideA, sidechoice);
+        }
+        else if(sidechoice.equals("C")){
+            System.out.println("what is side A? >>>");
+            sideA = input.nextDouble();
+            System.out.println("what is side B? >>>");
+            sideB = input.nextDouble();
+            calculateSideC(sideA, sideB, sidechoice);
+        }
+
+    }
+    public static void calculateSideA_B(double side1, double side2, String choice){
+        double aORb;
+        aORb = Math.sqrt(side1*side1 - side2*side2);
+        System.out.println("Side " + choice + " = " + aORb);
+    }
+    public static void calculateSideC(double side1, double side2, String choice){
+        double c;
+        c = Math.sqrt(side1*side1 + side2*side2);
+        System.out.println("Side " + choice + " = " + c);
+
+    }
+    public static void triangleDoesNotExist(){
+        System.out.println("That side does not exist.");
+    }
 }
